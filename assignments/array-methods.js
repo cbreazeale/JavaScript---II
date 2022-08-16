f7386399914445c3bf00d48baeaef71461933607
clay2 <clay@DESKTOP-B145MCP.localdomain>
2022-08-16 14:18:41 -0500

    getAllCaps complete

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -58,17 +58,44 @@ const runners = [{"id":1,"first_name":"Charmain","last_name":"Seiler","email":"c
 // ==== Challenge 1: Use .forEach() ====
 // The event director needs both the first and last names of each runner for their running bibs.  Combine both the first and last names into a new array called fullName.
 let fullName = [];
-console.log(fullName);
+// console.log(fullName);
+
+function getFullName(runners){
+    runners.forEach(el => {
+        fullName.push(el.first_name + ' ' + el.last_name)
+    });
+    return fullName
+}
+// console.log(getFullName(runners))
+
 
 // ==== Challenge 2: Use .map() ====
 // The event director needs to have all the runner's first names converted to uppercase because the director BECAME DRUNK WITH POWER. Convert each first name into all caps and log the result
 let allCaps = [];
-console.log(allCaps);
+// console.log(allCaps);
+
+function getAllCaps(runners){
+    runners.map(el => {
+        allCaps.push(el.first_name.toUpperCase())
+    })
+    return allCaps;
+}
+// console.log(getAllCaps(runners))
+
 
 // ==== Challenge 3: Use .filter() ====
 // The large shirts won't be available for the event due to an ordering issue.  Get a list of runners with large sized shirts so they can choose a different size. Return an array named largeShirts that contains information about the runners that have a shirt size of L and log the result
 let largeShirts = [];
-console.log(largeShirts);
+// console.log(largeShirts);
+
+function getLargeShirts(runners){
+    runners.filter(el => {
+        if(el.shirt_size == 'L'){
+             largeShirts.push(el)
+        }
+    })
+    return 
+}
 
 // ==== Challenge 4: Use .reduce() ====
 // The donations need to be tallied up and reported for tax purposes. Add up all the donations into a ticketPriceTotal array and log the result
@@ -85,13 +112,6 @@ console.log(ticketPriceTotal);
 // one or many of the array methods listed above.
 
 // Problem 1
-function getFullName(runners){
-    runners.forEach(el => {
-        fullName.push(el.first_name + ' ' + el.last_name)
-    });
-    return fullName
-}
-// console.log(getFullName(runners))
 
 // Problem 2
 
